d3d8528c957b953738d12cb5c7c8c0e64294a5b1
Helysson Cavalcante Bentes do N. <69604079+HelyssonBN3040@users.noreply.github.com>
2024-07-02 14:19:38 +0000

    instalação express e criação de rotas

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello world!")
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.get('/oi', (req, res)  => {
+  res.send('Nova rota: oi')
+})
+
+app.get('/outra-rota', (req, res)  => {
+  res.send('Outra rota: olá')
+})
+
+app.listen(3000)
