f73d5fad586f41f74a05ba3f207f0c63fcc6071b
Helysson Cavalcante Bentes do N. <69604079+HelyssonBN3040@users.noreply.github.com>
2024-07-03 14:10:29 +0000

    criação de endpoints

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 const express = require('express')
 const app = express()
 
+const list = ['Rick Sanchez', 'Morty Smith', 'Summer Smith']
+
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
-app.get('/oi', (req, res)  => {
-  res.send('Nova rota: oi')
+
+// consulta da lista
+app.get('/item', function (req, res) {
+  res.send(list)
 })
 
-app.get('/outra-rota', (req, res)  => {
-  res.send('Outra rota: olá')
+// Sinalizamos para o Express que vamos usar JSON no Body
+app.use(express.json())
+
+
+// Create - [POST] /item
+app.post('/item', function (req, res) {
+  // obtemos o nome da request
+  const item = req.body.nome
+
+  // Inseriu o nome na lista
+  list.push(item)
+
+  // Enviado uma mensagem com sucesso
+  res.send('Item criado com sucesso!')
 })
 
 app.listen(3000)
