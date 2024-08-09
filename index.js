4c2f296fdff4f37ecb634c61b61c5bb14b93ac27
HelyssonBN3040 <helyssoncavalcante.com@gmail.com>
2024-08-08 22:58:20 -0300

    Correção cors

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
 const express = require('express')
-const cors = require('cors')
 const { MongoClient, ObjectId } = require('mongodb')
+const cors = require('cors') // Importa o pacote cors
 const app = express()
-
-app.use(cors())
 
 const dbURL = 'mongodb+srv://admin:gmEmuTz9i8T0vCYR@cluster0.scztp2d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 const dbName = 'ocean-jornada-backend'
 
 const client = new MongoClient(dbURL)
 
-
 async function main() {
   console.log('Conectando ao banco de dados...')
   await client.connect()
   console.log('Banco de dados conectado com sucesso!')
 
+  // Configura o CORS para permitir requisições do frontend hospedado no Vercel
+  app.use(cors(
+    {
+      origin: 'https://register-clients.vercel.app', // substitua pela URL do seu frontend
+      methods: ['GET', 'POST', 'PUT', 'DELETE'], // métodos permitidos
+    },
+    {
+      origin: 'http://localhost:5173',
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    }
+  ))
 
   app.get('/', function (req, res) {
     res.send('Hello World')
@@ -23,7 +31,6 @@ async function main() {
 
   const db = client.db(dbName)
   const collection = db.collection('item')
-
 
   // consulta da lista
   app.get('/item', async function (req, res) {
@@ -34,27 +41,19 @@ async function main() {
   // Sinalizamos para o Express que vamos usar JSON no Body
   app.use(express.json())
 
-
   // Create - [POST] /item
   app.post('/item', async function (req, res) {
-    // obtemos o nome da request
     const item = req.body
-    //inserimos o item na collection
     await collection.insertOne(item)
-    // Enviado uma mensagem com sucesso
     res.send(item)
   })
-
 
   // Read By id - [GET] /item/:id
   app.get('/item/:id', async function (req, res) {
-    // acesso ao parametros da rota id
     const id = req.params.id
-    //acessar o id, através da rota
-    const item = await collection.findOne({_id: new ObjectId(id)})
+    const item = await collection.findOne({ _id: new ObjectId(id) })
     res.send(item)
   })
-
 
   // Update - PUT
   app.put('/item/:id', async function (req, res) {
@@ -62,28 +61,24 @@ async function main() {
     const novoItem = req.body
 
     await collection.updateOne(
-      {_id: new ObjectId(id)},
-      {$set: novoItem}
+      { _id: new ObjectId(id) },
+      { $set: novoItem }
     )
     res.send(`Valor atualizado: ${id}`)
   })
 
-  //Delete - [DELETE] /item/:id
-  app.delete('/item/:id', async function(req, res){
-    // acessando o parametro ID
+  // Delete - [DELETE] /item/:id
+  app.delete('/item/:id', async function (req, res) {
     const id = req.params.id
 
-    // Remove o item da collection pelo ObjectID
     await collection.deleteOne(
-      {_id: new ObjectId(id)}
+      { _id: new ObjectId(id) }
     )
-    
-    // Enviamos o item da collection pelo ObjectID
-    res.send(`Item ${id}, foi removido com sucesso!`)
+
+    res.send(`Item ${id} foi removido com sucesso!`)
   })
 
   app.listen(5152)
 }
-
 
 main()
